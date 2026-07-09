2cd195fc1faa7cf7504c3c0a5d6b684d4edf2fba
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-09 23:12:27 +0500

    Add database connection setup and data source configuration

---
+++ config/database.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const connectDatabase = ()=>{
+    mongoose.connect
+    (process.env.MONGO_URL
+    ).then( (data)=>{
+        console.log(`MongoDb connected with server ${data.connection.host}`)
+    }).catch((err)=>{
+        console.log(err)
+    })
+}
+
+export default connectDatabase;
