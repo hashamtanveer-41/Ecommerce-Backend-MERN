6b948822da8713b3c8f63bcccce1a888ecb294e3
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-10 03:13:22 +0500

    Add handling for CastError in error middleware

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,6 +1,14 @@
+import ErrorHandler from "../utils/errorHandler.js";
+
 const error = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error"
+
+    if (err.name === "CastError"){
+        const message = "Resource not found";
+        err = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         message: err.message,
