9caa7e8bbd6a7f30d6f310e374de0c292c06edc2
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-10 03:12:53 +0500

    Add ApiFeatures class for advanced query handling and filtering

---
+++ utils/apiFeatures.js
@@ -0,0 +1,32 @@
+class ApiFeatures{
+    constructor(query, queryStr) {
+        this.query = query;
+        this.queryStr  = queryStr;
+    }
+    search(){
+        const keyword = this.queryStr.keyword ?{
+            name: {
+                $regex: this.queryStr.keyword,
+                $options: "i",
+
+            }
+        }:{}
+        this.query = this.query.find({...keyword});
+        return this;
+    }
+
+    filter(){
+        const queryCopy = {...this.queryStr};
+        // Remove some fields
+        const removeFields = ["keyword", "page", "limit"];
+        removeFields.forEach(key=>delete queryCopy[key])
+        console.log(queryCopy)
+        let queryStr = JSON.stringify(queryCopy);
+        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (key)=>`$${key}`)
+        console.log(queryStr)
+        this.query  = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+}
+
+export default ApiFeatures;
