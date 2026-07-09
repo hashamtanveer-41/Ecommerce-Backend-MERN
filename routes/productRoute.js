9e69c4431b4ff63f59416949a187f16f8715905f
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-09 23:12:48 +0500

    Add product CRUD routes to productRoute.js

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,8 +1,18 @@
 import express from "express";
-import {getAllProducts} from "../controllers/productController.js";
+import {
+    createProduct,
+    deleteProduct,
+    getAllProducts,
+    productDetails,
+    updateProduct
+} from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.route("/products").get(getAllProducts);
-
+router.route("/products/new").post(createProduct);
+router.route("/product/:id")
+    .put(updateProduct)
+    .delete(deleteProduct)
+    .get(productDetails)
 export default router;
