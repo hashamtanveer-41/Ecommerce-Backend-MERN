ac1bfe0fb2ebaaa622faf7da5d8450400b540fae
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-09 23:29:35 +0500

    Add error handling middleware to app.js

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import product from "./routes/productRoute.js";
+import error from "./middlewares/error.js";
 
 const app = express();
 app.use(express.json())
 app.use("/api/v1", product)
-
+app.use(error)
 export default app;
