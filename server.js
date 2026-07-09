837903e865d5bc9253d973ff2942408bed58080f
Hasham Tanveer <hashamtanvr41@gmail.com>
2026-07-10 03:13:32 +0500

    Add uncaught exception and unhandled promise rejection handlers in server.js

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ import app from './app.js'
 import dotenv from "dotenv";
 import connectDatabase from "./config/database.js";
 
+// Handling Uncaught Exceptions
+process.on("uncaughtException", (err)=>{
+    console.log(`Error: ${err}`)
+    console.log("Shutting Down the server due to Uncaught Promise Rejection")
+        process.exit(1);
+})
+
 dotenv.config({path: ".env"})
 connectDatabase();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, ()=>{
+const server  = app.listen(PORT, ()=>{
     console.log(`Server running on http://localhost:${PORT}`)
 });
+
+// unhandled Promise Rejection
+process.on("unhandledRejection", err=>{
+    console.log(`Error: ${err}`)
+    console.log("Shutting Down the server due to Unhandled Promise Rejection")
+    server.close(()=>{
+        process.exit(1);
+    })
+})
